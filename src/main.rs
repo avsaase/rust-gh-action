a2bb3e481bdc2948a7ea57460583800f34627725
Alexander van Saase <avsaase@gmail.com>
2023-05-20 16:00:04 +0200

    Clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
-use std::{env, fs::write, process::exit};
+use std::{env, error::Error, fs::write, process::exit};
 
 use ureq::serde_json;
 
-fn main() {
-    let output_path = "output".to_string();
+const OUTPUT_PATH: &str = "output";
 
+fn main() {
     let args: Vec<String> = env::args().collect();
     let url = &args[1];
 
     println!("Making GET request to {}", &url);
-
     match ureq::get(url).call() {
         Ok(response) => match response.into_json::<serde_json::Value>() {
-            Ok(json) => write(output_path, format!("response={json}")).unwrap(),
-            Err(error) => {
-                eprint!("Error decoding response: {}", error);
-                write(output_path, format!("error={error}")).unwrap();
-                exit(1);
-            }
+            Ok(json) => write(OUTPUT_PATH, format!("response={json}")).unwrap(),
+            Err(error) => return_error(error),
         },
-        Err(error) => {
-            eprintln!("Error: {error}");
-            write(output_path, format!("error={error}")).unwrap();
-            exit(1);
-        }
+        Err(error) => return_error(error),
     }
+}
+
+fn return_error(error: impl Error) {
+    eprintln!("Error: {}", error);
+    write(OUTPUT_PATH, format!("error={}", error)).unwrap();
+    exit(1);
 }
